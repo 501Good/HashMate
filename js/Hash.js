bb33e22706c6858dea59df2620913f738f9cdf19
501Good <milintsevich@gmail.com>
2019-01-20 17:42:51 +0200

    Added Bloom Filters

--- js/Hash.js
+++ js/Hash.js
@@ -2,6 +2,11 @@ var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 var size = 0;
 var initial_x;
+var FNVPRIME = 0x01000193;
+var FNVINIT = 0x811c9dc5;
+var seed = Math.floor(Math.random() * 2e32);
+var bloomCounter = 1;
+var greens = new Set([]);
 
 
 function selectAlgorithm() {
@@ -28,6 +33,32 @@ function selectAlgorithm() {
 
     }
 
+    else if (selection == "bloom") {
+        $("#insert").click(function() {
+            ctx.clearRect(300, 150, 500, 300);
+            var value = document.getElementById('value').value;
+            var hash1 = murmurhash3_32_gc(value, seed) % size;
+            var hash2 = fnv1s(value) % size;
+
+            ctx.beginPath();
+            ctx.rect(initial_x + hash1 * 50, 50, 50, 50);
+            ctx.rect(initial_x + hash2 * 50, 50, 50, 50);
+            ctx.fillStyle = 'green';
+            ctx.fill();
+            ctx.stroke();
+            ctx.fillStyle = 'black';
+
+            var desc = "String: " + value + "<br/>Murmur hash: " + hash1 + "<br/>FNV hash: " + hash2;
+            $("#description").html(desc);
+            if (bloomCounter == 1) {
+                $("#bloomTable").append('<thead><tr><th scope="col">#</th><th scope="col">String</th><th scope="col">Murmur</th><th scope="col">FNV</th></tr></thead>');
+            }
+            $("#bloomTableBody").append('<tr onclick="selectBloom($(this))" class="bloomRow"><th scope="row">' + bloomCounter + '</th><td id="val">' + value + '</td><td id="hash1">' + hash1 + '</td><td id="hash2">' + hash2 + "</td></tr>");
+            bloomCounter++;
+            greens.add(hash1);
+            greens.add(hash2);
+        });
+    }
 
 }
 
@@ -55,6 +86,14 @@ function createTable() {
     console.log(size);
     if (size == 20) { document.getElementById("error").innerHTML = "Please choose the size of hash table"; }
     else {
+        if ($("#selectType").val() == "bloom") {
+            $("#canvas").css("height", "200");
+            $("#canvas").attr("height", "200");
+            bloomCounter = 1;
+        } else {
+            $("#canvas").css("height", "500");
+            $("#canvas").attr("height", "500");
+        }
         hash_table = new HashTable(size);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         var table_size = size * 50;
@@ -99,6 +138,38 @@ function drawText(text){
 
 
 }
+
+
+// http://stackoverflow.com/questions/1240408/reading-bytes-from-a-javascript-string/1242596#1242596
+function stringToBytes(str) {
+    var ch, st, re = [];
+    for (var i = 0; i < str.length; i++) {
+        ch = str.charCodeAt(i);  // get char
+        st = [];                 // set up "stack"
+        do {
+        st.push( ch & 0xFF );  // push byte to stack
+        ch = ch >> 8;          // shift value down by 1 byte
+        }
+        while ( ch );
+        // add stack contents to result
+        // done because chars have "wrong" endianness
+        re = re.concat( st.reverse() );
+    }
+    // return an array of bytes
+    return re;
+}
+
+
+function fnv1s(str) {
+    var bytes = stringToBytes(str);
+    var hash = FNVINIT;
+    for (var i=0; i < bytes.length; i++) {
+      hash *= FNVPRIME;
+      hash ^= bytes[i];
+    }
+    return Math.abs(hash);
+}
+
 
 function simpleHashing() {
     validate()
@@ -214,6 +285,73 @@ function quadraticProbing() {
     drawNumber(value, hash, 1)
 
 }
+
+
+function selectBloom(el) {
+    console.log(el);
+    var hash1 = parseInt(el.find("td#hash1").text());
+    var hash2 = parseInt(el.find("td#hash2").text());
+    greens.forEach(function(e) {
+        if (e != hash1 || e != hash2) {
+            ctx.clearRect(initial_x + e * 50, 50, 50, 50); 
+            ctx.clearRect(initial_x + e * 50, 50, 50, 50); 
+            ctx.beginPath();
+            ctx.rect(initial_x + e * 50, 50, 50, 50); 
+            ctx.rect(initial_x + e * 50, 50, 50, 50); 
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "#dee2e6";
+            ctx.fillStyle = 'green';
+            ctx.fill();
+            ctx.stroke();
+            ctx.fillStyle = 'black';
+        }
+    });
+    ctx.beginPath();
+    ctx.rect(initial_x + hash1 * 50, 50, 50, 50); 
+    ctx.rect(initial_x + hash2 * 50, 50, 50, 50); 
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "red";
+    ctx.fillStyle = 'green';
+    ctx.fill();
+    ctx.stroke();
+    ctx.fillStyle = 'black';
+}
+
+
+jQuery(document).ready(function($) { 
+    $(".table > tbody > tr").click(function() {
+        window.location = "hello";
+        $(this).addClass("chosen");
+        var hash1 = parseInt($("tr.chosen").find("td#hash1.chosen").text());
+        var hash2 = parseInt($("tr.chosen").find("td#hash2.chosen").text());
+        $("#description").text(hash1 + " " + hash2);
+        ctx.beginPath();
+        ctx.rect(initial_x + hash1 * 50, 50, 50, 50); 
+        ctx.rect(initial_x + hash2 * 50, 50, 50, 50); 
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "red";
+        ctx.fillStyle = 'green';
+        ctx.fill();
+        ctx.stroke();
+        ctx.fillStyle = 'black';
+    }, function() {
+        var hash1 = parseInt($("tr.chosen").find("td#hash1").text());
+        var hash2 = parseInt($("tr.chosen").find("td#hash2").text());
+        $("#description").text(hash1 + " " + hash2);
+        ctx.clearRect(initial_x + hash1 * 50, 50, 50, 50); 
+        ctx.clearRect(initial_x + hash2 * 50, 50, 50, 50); 
+        ctx.beginPath();
+        ctx.rect(initial_x + hash1 * 50, 50, 50, 50); 
+        ctx.rect(initial_x + hash2 * 50, 50, 50, 50); 
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "#dee2e6";
+        ctx.fillStyle = 'green';
+        ctx.fill();
+        ctx.stroke();
+        ctx.fillStyle = 'black';
+        $(this).removeClass("chosen");
+    });
+});
 
 window.onload = function () {
 }; 
