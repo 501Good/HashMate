efb77ff3d5833e01859c8d575b5e084e4573a8d6
501Good <milintsevich@gmail.com>
2019-01-22 09:15:33 +0200

    Added Universal Hashing

--- js/Hash.js
+++ js/Hash.js
@@ -34,6 +34,10 @@ function selectAlgorithm() {
             algorithm = "bloom";
             break;
 
+        case "universal":
+            algorithm = "universal";
+            break;
+
         default:
             algorithm = "null";
 
@@ -90,12 +94,162 @@ function executeAlgorithm() {
             bloom();
             break;
 
+        case "universal":
+            $('#canvas').hide();
+            var prime = 31;
+            var value = parseInt($("#value").val());
+            var tableSize = parseInt($("#myRange").val());
+            var collisions = {};
+            for (var i = 0; i < tableSize; i += 1) {
+                collisions[i] = 0;
+            }
+            var hash = 0;
+            for (var i = 0; i < 100; i += 1) {
+                hash = universalHashing(Math.floor(Math.random() * 2e32), prime, tableSize);
+                collisions[hash] += 1;
+            }
+
+            var sample = [];
+
+            for (key in collisions) {
+                sample.push({
+                    number: key,
+                    value: collisions[key]
+                })
+            }
+
+            console.log(sample);
+
+            drawBarChart(sample, tableSize);
+
         default:
 
     }
 }
 
 
+function drawBarChart(sample, tableSize) {
+    const svg = d3.select('svg');
+    const svgContainer = d3.select('#svg-container');
+    
+    const margin = 80;
+    const width = 1000 - 2 * margin;
+    const height = 600 - 2 * margin;
+
+    const chart = svg.append('g')
+        .attr('transform', `translate(${margin}, ${margin})`);
+
+    const xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(sample.map((s) => s.number))
+        .padding(0.4)
+    
+    const yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, tableSize]);
+
+    const makeYLines = () => d3.axisLeft()
+        .scale(yScale)
+
+    chart.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale));
+
+    chart.append('g')
+        .call(d3.axisLeft(yScale));
+
+        chart.append('g')
+        .attr('class', 'grid')
+        .call(makeYLines()
+          .tickSize(-width, 0, 0)
+          .tickFormat('')
+        )
+  
+    const barGroups = chart.selectAll()
+    .data(sample)
+    .enter()
+    .append('g')
+
+    barGroups
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', (g) => xScale(g.number))
+    .attr('y', (g) => yScale(g.value))
+    .attr('height', (g) => height - yScale(g.value))
+    .attr('width', xScale.bandwidth())
+    .on('mouseenter', function (actual, i) {
+        d3.selectAll('.value')
+        .attr('opacity', 0)
+
+        d3.select(this)
+        .transition()
+        .duration(300)
+        .attr('opacity', 0.6)
+        .attr('x', (a) => xScale(a.number) - 5)
+        .attr('width', xScale.bandwidth() + 10)
+
+        const y = yScale(actual.value)
+
+        line = chart.append('line')
+        .attr('id', 'limit')
+        .attr('x1', 0)
+        .attr('y1', y)
+        .attr('x2', width)
+        .attr('y2', y)
+
+    })
+    .on('mouseleave', function () {
+        d3.selectAll('.value')
+        .attr('opacity', 1)
+
+        d3.select(this)
+        .transition()
+        .duration(300)
+        .attr('opacity', 1)
+        .attr('x', (a) => xScale(a.number))
+        .attr('width', xScale.bandwidth())
+
+        chart.selectAll('#limit').remove()
+        chart.selectAll('.divergence').remove()
+    })
+
+    barGroups 
+    .append('text')
+    .attr('class', 'value')
+    .attr('x', (a) => xScale(a.number) + xScale.bandwidth() / 2)
+    .attr('y', (a) => yScale(a.value) + 30)
+    .attr('text-anchor', 'middle')
+    
+    svg
+    .append('text')
+    .attr('class', 'label')
+    .attr('x', -(height / 2) - margin)
+    .attr('y', margin / 2.4)
+    .attr('transform', 'rotate(-90)')
+    .attr('text-anchor', 'middle')
+    .text('Number of collisions')
+
+    svg.append('text')
+    .attr('class', 'label')
+    .attr('x', width / 2 + margin)
+    .attr('y', height + margin * 1.7)
+    .attr('text-anchor', 'middle')
+    .text('Hash bucket')
+
+    svg.append('text')
+    .attr('class', 'title')
+    .attr('x', width / 2 + margin)
+    .attr('y', 40)
+    .attr('text-anchor', 'middle')
+    .text('Number of collisions with the Universal Hashing function')
+
+    svg.append('text')
+    .attr('class', 'source')
+    .attr('x', width - margin / 2)
+    .attr('y', height + margin * 1.7)
+    .attr('text-anchor', 'start')
+    .text('Source: HashMate, 2019')
+}
 
 
 function validate() {
@@ -139,6 +293,12 @@ function createTable() {
             bloomCounter = 1;
             $("#bloomTable").empty();
             greens = new Set([]);
+        }
+        else if ($("#selectType").val() == "universal") {
+            $("#canvas").css("height", "0");
+            $("#canvas").attr("height", "0");
+            $("#canvas").css("width", "0");
+            $("#canvas").attr("width", "0");
         } else {
             $("#canvas").css("height", "500");
             $("#canvas").attr("height", "500");
@@ -540,6 +700,35 @@ function selectBloom(el) {
     ctx.fill();
     ctx.stroke();
     ctx.fillStyle = 'black';
+}
+
+
+function universalHashing(key, prime, tableSize) {
+    var a = Math.floor(Math.random() * (prime));
+    var b = Math.floor(Math.random() * (prime - 1) + 1);
+    var hash = ((a * key + b) % prime) % tableSize;
+    return hash;
+}
+
+
+function isPrime(num) {
+    if (num <= 1) {
+        return true;
+    } else if (num <= 3) {
+        return true;
+    } else if (num % 2 === 0 || num % 3 === 0) {
+        return false;
+    }
+
+    let i = 5;
+    while (i * i <= num) {
+        if (num % i === 0 || num % (i + 2) === 0) {
+          return false;
+        }
+        i += 6;
+    }
+
+    return true;
 }
 
 
